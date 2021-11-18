4b843316f3ed739b531d2ff379f1bb83e26bb8b0
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-18 12:54:12 +0100

    Fixed app page useEffecy

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,17 @@
 import "../styles/global.css";
 import { useEffect, useState } from "react";
-import Login from "@/components/Login/Login";
+import Login from "../components/Login/Login";
 
-import Header from "@/components/Header/Header";
+import Header from "../components/Header/Header";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   const [isThereUser, setIsThereUser] = useState(false);
 
   useEffect(() => {
-    const getUser = JSON.parse(localStorage.getItem("user"));
+    const user = JSON.parse(localStorage.getItem("user"));
     if (user) {
       setIsThereUser(true);
-      setUser(getUser);
     } else {
       setIsThereUser(false);
     }
